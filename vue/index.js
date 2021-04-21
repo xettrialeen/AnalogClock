d73a0e018b5ad70ed9b4ed83c1ff066025c89968
Xettri Aleen <pandapubgnp1@gmail.com>
2021-04-21 13:10:55 +0545

    ggg

---
+++ vue/index.js
@@ -0,0 +1,25 @@
+const clock = new Vue({
+  el: "#clock",
+  data: {
+    message: "hello world",
+  },
+  methods: {},
+  mounted: function () {
+    setInterval(setClock, 1000);
+    const hourHand = document.querySelector("[data-hour]");
+    const minuteHand = document.querySelector("[data-minute]");
+    const secondHand = document.querySelector("[data-second]");
+    function setClock() {
+      const currentDate = new Date();
+      const secondRatio = currentDate.getSeconds() / 60;
+      const minuteRatio = (secondRatio + currentDate.getMinutes()) / 60;
+      const hourRatio = (minuteRatio + currentDate.getHours()) / 12;
+      setRotation(secondHand, secondRatio);
+      setRotation(minuteHand, minuteRatio);
+      setRotation(hourHand, hourRatio);
+    }
+    function setRotation(element, rotationRatio) {
+      element.style.setProperty("--rotation", rotationRatio * 360);
+    }
+  },
+});
